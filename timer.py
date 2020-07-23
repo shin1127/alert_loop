704e1cd33793e3a2311e07061c3aea5d6752f2b8
shin1127 <56531152+shin1127@users.noreply.github.com>
2020-07-23 16:02:02 +0900

    Update timer.py

--- timer.py
+++ timer.py
@@ -3,12 +3,15 @@ import pygame
 import time
 
 filename = 'se_maoudamashii_onepoint26.mp3'  # 再生したいmp3ファイル
+interval_time = 30  # 再生間隔を秒数で指定する
+
+
 pygame.mixer.init()
 pygame.mixer.music.load(filename)  # 音源を読み込み
 mp3_length = mp3(filename).info.length  # 音源の長さ取得
 
 while True:
     pygame.mixer.music.play(1)
-    time.sleep(30)
+    time.sleep(interval_time)
 
-pygame.mixer.music.stop()  # 音源の長さ待ったら再生停止
+pygame.quit()
